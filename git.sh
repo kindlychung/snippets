00d253ea0e8c1e5027bf635c591dc6f2d499056e
Kaiyin <kindlychung@gmail.com>
2017-03-30 11:49:22 +0200

    heroku push different branch

--- git.sh
+++ git.sh
@@ -3,3 +3,7 @@ git push origin  --delete branch_to_be_deleted
 
 # store/save passwords
 git config credential.helper store
+
+# Yes, pushing to a branch named differently that your current one is possible using the syntax:
+git push heroku other:master
+
